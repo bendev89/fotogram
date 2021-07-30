cb79c4da772b9145c9784768eff160fbf1f7ef27
Benoit <ndiayeben89@gmail.com>
2021-07-31 01:27:21 +0200

    Récupération et affichage des images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import UploadForm from './components/UploadForm';
 import Title from './components/Title';
+import ImageGrid from './components/ImageGrid';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
